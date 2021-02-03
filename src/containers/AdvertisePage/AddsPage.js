1e9d46ceed7ffcb10da4fe374ba14ea9ea2f47af
Nasir <movlamovnasir2000@gmail.com>
2021-02-03 11:39:08 +0400

    newnew

--- src/containers/AdvertisePage/AddsPage.js
+++ src/containers/AdvertisePage/AddsPage.js
@@ -10,13 +10,21 @@ import adImage from "../../assets/images/component/element/adImage.png"
 import Ad from '../../components/Ad';
 
 function AddsPage(props) {
+
+    document.title = " Ustatap.net Elanlar"
+
     const [Adds, setAdds] = useState([])
     var url = window.location.href;
     var id = url.substring(url.lastIndexOf('/') + 1 );
+    var numId = url.substring(url.lastIndexOf('/') + 1 );
+    const [jobCategoryApi, setJobCategoryApi] = useState([0])
     const [SelectedAd, setSelectedAd] = useState(0)
     const [latestAdApi, setlatestAdApi] = useState([0])
+    const jobCategory = []
+
     const AllAd = []
-    AllAd.push( latestAdApi.map(ad => <Ad name={ ad.title} costumer={ad.description} address={ad.city} date={ad.updated_at} view="1258" image={ad.images} id={ad.id}/>)  ) 
+    jobCategoryApi.map((category) => jobCategory.push(category.category_id))
+    
     useEffect(() => 
     {
         axios.get(`http://ustatap.testjed.me/singlead/${id}`) 
@@ -26,6 +34,30 @@ function AddsPage(props) {
             .then((res) =>  (setlatestAdApi(res.data) ))
 
     } , [])
+
+    latestAdApi.map(
+        ad => 
+            {
+                console.log(numId);
+                if(numId === 'elanlar')
+                {
+                    AllAd.push(<Ad name={ ad.title} costumer={ad.description} address={ad.city} date={ad.updated_at} view="1258" image={ad.images} id={ad.id}/>)  
+                }
+                if(parseInt(numId) !== 'elanlar')
+                {
+                    if(ad.category_id===parseInt(numId))
+                    {
+                        AllAd.push(<Ad name={ ad.title} costumer={ad.description} address={ad.city} date={ad.updated_at} view="1258" image={ad.images} id={ad.id}/>) 
+                    }
+                }
+                
+                else{}
+            }
+        
+    ) 
+
+
+
     return (
         <div className="addsPage">
             <div className="generalCont">
--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -38,7 +38,7 @@ import {Link} from "react-router-dom"
 import FadeIn from 'react-fade-in';
 function HomePage(props) {
 
-
+    document.title = " Ustatap.net Əsas Səhifə"
 
     const latestAd = []
     const masters = []
@@ -68,6 +68,37 @@ function HomePage(props) {
 
     },[] )
 
+  
+    
+    
+    
+
+    latestAdApi.map((ad) => latestAd.push(<Ad name={ ad.title} costumer={ad.description} address={"Baku"} date={ad.updated_at} view={1258} image={ad.images} id={ad.id}/>)  ) 
+    
+    jobCategoryApi.map((category) => jobCategory.push(
+        <button className="contForAside" onClick={() => clickHandler(category.id)}>
+            <div className="workTypeCont">
+                <img  width='30px' height='auto' src={category.icon} alt={props.name}/>
+                <p>{category.name}</p>
+                <button ><img  src={arrDown} alt=""/></button>
+            </div>
+            <div className="workDescCont" id={`wD${category.id}`} >
+                <p><Link to={`/ustalar/${category.id}`}>Ustalar <span>({23})</span></Link> </p>
+                <p><Link to={`/elanlar/${category.id}`}>Elanlar <span>({22})</span></Link></p>
+                <p><Link to={`/shirketler/${category.id}`}>Şirkətlər <span>({11})</span></Link></p>
+            </div>
+        </button>
+    ))
+
+
+    latestAdApi.map((ad, index ) => {if(ad.category_id == 3){santexnika.push(<Ad name={ ad.title} costumer={ad.description} address={"Baku"} date={ad.updated_at} view="1258" image={ad.images} id={ad.id}/>)}}) 
+    latestAdApi.map((ad, index ) => {if(ad.category_id == 4){electric.push(<Ad name={ ad.title} costumer={ad.description} address={ad.city} date={ad.updated_at} view="1258" image={ad.images} id={ad.id}/>)}}) 
+    
+    MasterApi.map(master =>  {if(master.vip !== 1){ masters.push(<Ad2 name={master.name} job={master.surname} address={master.city} image={master.image} numberStar={master.rating} id={master.id} rating={master.rating}/>)}})
+    MasterApi.map(master =>  {if(master.vip === 1){ vipMasters.push(<VipAd2 name={master.name} job={master.surname} address={master.city} image={master.image} numberStar={master.rating} id={master.id } rating={master.rating}/> )}})
+    CompanyApi.map( company =>  { if(company.vip !== 1){ companies.push(<Ad3 id={company.id} numberStar={company.rating} image={company.image} name={company.company_name} location={company.company_adress} description={company.description}/>)}} )
+    CompanyApi.map( company =>  {if(company.vip === 1){ vipCompanies.push(<VipAd3 id={company.id} numberStar={company.rating} image={company.image} name={company.company_name} location={company.company_adress} description={company.description}/>)}} )
+    
     const  clickHandler = (number) => {
         if (document.getElementById("wD" + number).style.display === "block") {
             document.getElementById("wD" + number).setAttribute("style" , "display:nonne")
@@ -77,20 +108,9 @@ function HomePage(props) {
             document.getElementById("wD" + number).setAttribute("style" , "display:block")
         }  
     }
-    
-    
-    
 
-    latestAdApi.map((ad) => latestAd.push(<Ad name={ ad.title} costumer={ad.description} address={"Baku"} date={ad.updated_at} view={1258} image={ad.images} id={ad.id}/>)  ) 
-    jobCategoryApi.map((category) => jobCategory.push(category.name))
-    latestAdApi.map((ad, index ) => {if(ad.category_id == 3){santexnika.push(<Ad name={ ad.title} costumer={ad.description} address={"Baku"} date={ad.updated_at} view="1258" image={ad.images} id={ad.id}/>)}}) 
-    latestAdApi.map((ad, index ) => {if(ad.category_id == 4){electric.push(<Ad name={ ad.title} costumer={ad.description} address={ad.city} date={ad.updated_at} view="1258" image={ad.images} id={ad.id}/>)}}) 
-    
-    MasterApi.map(master =>  {if(master.vip !== 1){ masters.push(<Ad2 name={master.name} job={master.surname} address={master.city} image={master.image} numberStar={master.rating} id={master.id} rating={master.rating}/>)}})
-    MasterApi.map(master =>  {if(master.vip === 1){ vipMasters.push(<VipAd2 name={master.name} job={master.surname} address={master.city} image={master.image} numberStar={master.rating} id={master.id } rating={master.rating}/> )}})
-    CompanyApi.map( company =>  { if(company.vip !== 1){ companies.push(<Ad3 id={company.id} numberStar={company.rating} image={company.image} name={company.company_name} location={company.company_adress} description={company.description}/>)}} )
-    CompanyApi.map( company =>  {if(company.vip === 1){ vipCompanies.push(<VipAd3 id={company.id} numberStar={company.rating} image={company.image} name={company.company_name} location={company.company_adress} description={company.description}/>)}} )
-    
+
+
     return (
         <div className="homePage">
             
@@ -103,200 +123,7 @@ function HomePage(props) {
             <main className="main"> 
             
                 <aside className="aside" id="aside">
-                    {/* 1 */}
-                    <button className="contForAside" onClick={() => clickHandler(1)}>
-                        <div className="workTypeCont">
-                            <img src={plumber} alt="santexnik"/>
-                            <p>Santexnik</p>
-                            <button ><img  src={arrDown} alt=""/></button>
-                        </div>
-                        <div className="workDescCont" id="wD1" >
-                            <p><Link to="/masters/santexnik">Ustalar <span>({23})</span></Link> </p>
-                            <p><Link to="/elanlar">Elanlar <span>({22})</span></Link></p>
-                            <p><Link to="/companies">Şirkətlər <span>({11})</span></Link></p>
-                        </div>
-                    </button>
-                    
-                    {/* 2 */}
-                    <button className="contForAside" onClick={() => clickHandler(2)} >
-                        <div className="workTypeCont">
-                            <img src={electricImg} alt=""/>
-                            <p>Elektirik</p>
-                            <button ><img src={arrDown} alt=""/></button>
-                        </div>
-                        <div className="workDescCont" id="wD2" >
-                            <p><Link to="/masters">Ustalar <span>({23})</span></Link></p>
-                            <p><Link to="/elanlar">Elanlar <span>({23})</span></Link></p>
-                            <p><Link to="/companies">Şirkətlər <span>({23})</span></Link></p>
-                        </div>
-                    </button>
-
-                    {/* 3 */}
-                    <button className="contForAside"  onClick={() => clickHandler(3)} >
-                        <div className="workTypeCont">
-                            <img src={wallpaper} alt=""/>
-                            <p>Divar Kağızı</p>
-                            <button><img src={arrDown} alt=""/></button>
-                        </div>
-                        <div className="workDescCont" id="wD3" >
-                            <p><Link to="/masters">Ustalar <span>({23})</span></Link></p>
-                            <p><Link to="/elanlar">Elanlar <span>({23})</span></Link></p>
-                            <p><Link to="/companies">Şirkətlər <span>({23})</span></Link></p>
-                        </div>
-                    </button>
-
-                    {/* 4 */}
-                    <button className="contForAside"  onClick={() => clickHandler(4)} >
-                        <div className="workTypeCont">
-                            <img src={windowDoor} alt=""/>
-                            <p>Qapı-Pəncərə</p>
-                            <button><img src={arrDown} alt=""/></button>
-                        </div>
-                        <div className="workDescCont" id="wD4" >
-                            <p><Link to="/masters">Ustalar <span>({23})</span></Link></p>
-                            <p><Link to="/elanlar">Elanlar <span>({23})</span></Link></p>
-                            <p><Link to="/companies">Şirkətlər <span>({23})</span></Link></p>
-                        </div>
-                    </button>
-
-                    {/* 5 */}
-                    <button className="contForAside"  onClick={() => clickHandler(5)} >
-                        <div className="workTypeCont">
-                            <img src={welder} alt=""/>
-                            <p>Qaynaqçı</p>
-                            <button><img src={arrDown} alt=""/></button>
-                        </div>
-                        <div className="workDescCont" id="wD5" >
-                            <p><Link to="/masters">Ustalar <span>({23})</span></Link></p>
-                            <p><Link to="/elanlar">Elanlar <span>({23})</span></Link></p>
-                            <p><Link to="/companies">Şirkətlər <span>({23})</span></Link></p>
-                        </div>
-                    </button>
-
-                    {/* 6 */}
-                    <button className="contForAside"  onClick={() => clickHandler(6)} >
-                        <div className="workTypeCont">
-                            <img src={plasterer} alt=""/>
-                            <p>Suvaqçı</p>
-                            <button><img src={arrDown} alt=""/></button>
-                        </div>
-                        <div className="workDescCont" id="wD6" >
-                            <p><Link to="/masters">Ustalar <span>({23})</span></Link></p>
-                            <p><Link to="/elanlar">Elanlar <span>({23})</span></Link></p>
-                            <p><Link to="/shirketler">Şirkətlər <span>({23})</span></Link></p>
-                        </div>
-                    </button>
-
-                    {/* 7 */}
-                    <button className="contForAside"  onClick={() => clickHandler(7)} >
-                        <div className="workTypeCont">
-                            <img src={metlag} alt=""/>
-                            <p>Metlaq</p>
-                            <button><img src={arrDown} alt=""/></button>
-                        </div>
-                        <div className="workDescCont" id="wD7" >
-                            <p><Link to="/masters">Ustalar <span>({23})</span></Link></p>
-                            <p><Link to="/elanlar">Elanlar <span>({23})</span></Link></p>
-                            <p><Link to="/companies">Şirkətlər <span>({23})</span></Link></p>
-                        </div>
-                    </button>
-
-                    {/* 8 */}
-                    <button className="contForAside"  onClick={() => clickHandler(8)} >
-                        <div className="workTypeCont">
-                            <img src={roof} alt=""/>
-                            <p>Dam Örtüyü</p>
-                            <button><img src={arrDown} alt=""/></button>
-                        </div>
-                        <div className="workDescCont" id="wD8" >
-                            <p><Link to="/masters">Ustalar <span>({23})</span></Link></p>
-                            <p><Link to="/elanlar">Elanlar <span>({23})</span></Link></p>
-                            <p><Link to="/companies">Şirkətlər <span>({23})</span></Link></p>
-                        </div>
-                    </button>
-
-                    {/* 9 */}
-                    <button className="contForAside"  onClick={() => clickHandler(9)} >
-                        <div className="workTypeCont">
-                            <img src={stonemaster} alt=""/>
-                            <p>Bənna</p>
-                            <button><img src={arrDown} alt=""/></button>
-                        </div>
-                        <div className="workDescCont" id="wD9" >
-                            <p><Link to="/masters">Ustalar <span>({23})</span></Link></p>
-                            <p><Link to="/elanlar">Elanlar <span>({23})</span></Link></p>
-                            <p><Link to="/companies">Şirkətlər <span>({23})</span></Link></p>
-                        </div>
-                    </button>
-
-                    {/* 10 */}
-                    <button className="contForAside"  onClick={() => clickHandler(10)} >
-                        <div className="workTypeCont">
-                            <img src={painter} alt=""/>
-                            <p>Malyar</p>
-                            <button><img src={arrDown} alt=""/></button>
-                        </div>
-                        <div className="workDescCont" id="wD10" >
-                            <p><Link to="/masters">Ustalar <span>({23})</span></Link></p>
-                            <p><Link to="/elanlar">Elanlar <span>({23})</span></Link></p>
-                            <p><Link to="/companies">Şirkətlər <span>({23})</span></Link></p>
-                        </div>
-                    </button>
-
-                    {/* 11 */}
-                    <button className="contForAside"  onClick={() => clickHandler(11)} >
-                        <div className="workTypeCont">
-                            <img src={floor} alt=""/>
-                            <p>Döşəmə</p>
-                            <button><img src={arrDown} alt=""/></button>
-                        </div>
-                        <div className="workDescCont" id="wD11" >
-                            <p><Link to="/masters">Ustalar <span>({23})</span></Link></p>
-                            <p><Link to="/elanlar">Elanlar <span>({23})</span></Link></p>
-                            <p><Link to="/companies">Şirkətlər <span>({23})</span></Link></p>
-                        </div>
-                    </button>
-
-                    {/* 12 */}
-                    <button className="contForAside"  onClick={() => clickHandler(12)} >
-                        <div className="workTypeCont">
-                            <img src={window} alt=""/>
-                            <p>Şüşə-Güzgü</p>
-                            <button><img src={arrDown} alt=""/></button>
-                        </div>
-                        <div className="workDescCont" id="wD12" >
-                            <p><Link to="/masters">Ustalar <span>({23})</span></Link></p>
-                            <p><Link to="/elanlar">Elanlar <span>({23})</span></Link></p>
-                            <p><Link to="/companies">Şirkətlər <span>({23})</span></Link></p>
-                        </div>
-                    </button>
-
-                    {/* 13 */}
-                    <button className="contForAside" onClick={() => clickHandler(13)} >
-                        <div className="workTypeCont">
-                            <img src={carpenter} alt=""/>
-                            <p>Dülgər</p>
-                            <button ><img src={arrDown} alt=""/></button>
-                        </div>
-                        <div className="workDescCont" id="wD13" >
-                            <p><Link to="/masters">Ustalar <span>({23})</span></Link></p>
-                            <p><Link to="/elanlar">Elanlar <span>({23})</span></Link></p>
-                            <p><Link to="/companies">Şirkətlər <span>({23})</span></Link></p>
-                        </div>
-                    </button>
-
-                    {/* 14 */}
-                    <button className="contForAside"  onClick={() => clickHandler(14)}  >
-                        <div className="workTypeCont">
-                            <button><img src={threeDots} alt=""/></button>
-                            <p className="differP">Digər</p>
-                        </div>
-                        <div className="workDescCont " id="wD14" >
-                            <p><Link to="/masters">Ustalar <span>({23})</span></Link></p>
-                            <p><Link to="/elanlar">Elanlar <span>({23})</span></Link></p>
-                            <p><Link to="/companies">Şirkətlər <span>({23})</span></Link></p>
-                        </div>
-                    </button>
+                    {jobCategory.reverse()}
                 </aside>
 
 
