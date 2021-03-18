8890a840f2c1fd8f70d2374e944d31e7d5bea988
Nasir <movlamovnasir2000@gmail.com>
2021-03-18 21:09:04 +0400

    new new

--- src/components/ReplyComment.js
+++ src/components/ReplyComment.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import "../assets/css/componentsCss/replyComment.css"
-import humanImg from "../assets/images/component/element/commentMainHuman.svg"
 import calendar from "../assets/images/component/element/calendarForComment.svg"
 
 function ReplyComment(props) {
-    const avatar = {
-        backgroundImage: `url(${humanImg})`
-    }
+    
     return (
             <div className="replyCommentCont">
-                <div className="avatar" style={avatar}></div>
                 <div  className="replyCommentAbout"> 
                         <p className="name">{props.namesubComment}<img src={calendar} alt="" /> {props.date} </p>
                         <hr className="lineForMain"/> 
